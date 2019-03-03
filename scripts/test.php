b866ab5bc7be96b94d5dfd499ea83147035b7b5c
EnTaro <43279456+EnTaroMe@users.noreply.github.com>
2019-03-03 20:44:48 +0300

    Update test.php

--- scripts/test.php
+++ scripts/test.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Main page</title>
+</head>
+<body>
+	<h1>Hello</h1>
+	<form method="post">
+		<textarea maxlength="255" name="content">
+			Type smth
+		</textarea>
+		<input type="submit">
+	</form>
+</body>
+</html>
 <?php
 // This is the data you want to pass to Python
 $data = $_POST['content'];
