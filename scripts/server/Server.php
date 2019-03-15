ba27b3926cf354639b27c438cf2367dda687279b
entaro <apan.kirill@yandex.ru>
2019-03-15 11:09:22 +0300

    Now you can sign in

--- scripts/server/Server.php
+++ scripts/server/Server.php
@@ -8,5 +8,38 @@
 
 class Server
 {
+    private $link = "";
 
+    #constructor
+    function __construct()
+    {
+        $localhost = "OtVinta";
+        $db = "PentaMailDB";
+        $user = "admin";
+        $password = "admin";
+
+        $this->link = mysqli_connect($localhost, $user, $password) or
+                      trigger_error(mysql_error(),E_USER_ERROR);
+
+        mysqli_query($this->link, "SET NAMES utf8;") or die(mysql_error());
+        mysqli_query($this->link, "SET CHARACTER SET utf8;") or die(mysql_error());
+
+        mysqli_select_db($this->link, $db);
+    }
+
+    function checkAccount($email, $password){
+        if(($email) &&($password))
+        {
+            $query = "SELECT * FROM Пользователи WHERE Email = '$email' 
+                                                 AND Пароль = '$password'";
+            $send_query = mysqli_query($this->link, $query);
+            $count = mysqli_num_rows($send_query);
+            if ($count > 0) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
 }
