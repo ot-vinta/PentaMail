58adcfea7ee64f233ac20a2489c007c4c6d6ce36
entaro <apan.kirill@yandex.ru>
2019-03-16 18:21:25 +0300

    Now you can sign in

---
+++ scripts/client/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <h1 align="center">Hello</h1>
+    </body>
+</html>
